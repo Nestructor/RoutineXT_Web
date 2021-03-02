6152d67a6254e607f62b4fd8116b09879921a58f
Nestor <nestor.bolanos102@alu.ulpgc.es>
2021-03-02 11:53:46 +0000

    Fixing web zoom appearence and beginning to develop HU-05

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './unregistered/home/home.component';
+import { LoginComponent } from './unregistered/login/login.component';
 
 const routes: Routes = [
   {
     path: "", component: HomeComponent
+  },
+  {
+    path: "Iniciar_Sesion", component: LoginComponent
   }
 ];
 
